c0b835b6b93fc206d5d7e0c9732735ac94cc1ee1
Ng0K <07_trendy_liable@icloud.com>
2024-09-10 23:26:46 +0900

    Lesson16

--- src/main/java/com/techacademy/CountryController.java
+++ src/main/java/com/techacademy/CountryController.java
@@ -3,7 +3,10 @@ package com.techacademy;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @RequestMapping("country")
@@ -22,4 +25,36 @@ public class CountryController {
         //country.list.htmlに画面遷移
         return "country/list";
     }
-}
+
+    //-----詳細画面-----//
+    @GetMapping(value= {"detail","/detail/{code}/"})
+    public String getCountry(@PathVariable(name="code", required=false)String code,Model model) {
+        //codeが指定されていたら検索結果、無ければからのクラスを設定
+        Country country = code != null ? service.getCountry(code) : new Country();
+        //Modelに登録
+        model.addAttribute("country", country);
+        //country/detail.htmlに画面遷移
+        return "country/detail";
+    }
+    //-----更新（追加）-----
+    @PostMapping("/detail")
+    public String postCountry(@RequestParam("code")String code, @RequestParam("name")String name, @RequestParam("population")int population, Model model) {
+        service.updateCountry(code, name, population);
+
+        return "redirect:/country/list";
+    }
+
+    @GetMapping("/delete")
+    public String deleteCountryForm(Model model) {
+        return "country/delete";
+    }
+
+    @PostMapping("/delete")
+    public String deleteCountry(@RequestParam("code")String code, Model model) {
+        service.deleteCountry(code);
+
+        return "redirect:/country/list";
+    }
+
+    }
+
